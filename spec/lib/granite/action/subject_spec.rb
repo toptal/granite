0234c2a34a2c9c0df59c55be29d5596de4b72fc9
Aitor Lopez Beltran <aitor.lopezbeltran@toptal.com>
2022-10-12 17:54:21 +0200

    [SCX-326] Update spec

--- spec/lib/granite/action/subject_spec.rb
+++ spec/lib/granite/action/subject_spec.rb
@@ -1,15 +1,15 @@
 RSpec.describe Granite::Action::Subject do
-  before do
-    stub_class(:action, Granite::Action) do
-      subject :student
-      attribute :comment, String
-    end
-  end
-
   let(:student) { Student.create! }
   let(:teacher) { Teacher.create! }
 
   describe '#initialize' do
+    before do
+      stub_class(:action, Granite::Action) do
+        subject :student
+        attribute :comment, String
+      end
+    end
+
     specify { expect { Action.new(comment: 'Comment') }.to raise_error Granite::Action::SubjectNotFoundError }
     specify { expect(Action.new(comment: 'Comment', subject: student).student).to eq(student) }
 
@@ -33,16 +33,16 @@ RSpec.describe Granite::Action::Subject do
     specify { expect(Action.new(student, comment: 'Comment').comment).to eq('Comment') }
   end
 
-  describe '::subject?' do
-    specify { expect(Action).to be_subject }
+  describe '.subject?' do
+    before { stub_class(:action, Granite::Action) }
 
-    context 'when Action does not define subject' do
-      before do
-        stub_class(:action, Granite::Action) do
-          attribute :comment, String
-        end
-      end
+    context 'when action defines subject' do
+      before { Action.subject(:student) }
 
+      specify { expect(Action).to be_subject }
+    end
+
+    context 'when action does not define subject' do
       specify { expect(Action).not_to be_subject }
     end
   end
