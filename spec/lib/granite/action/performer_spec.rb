ae403a9a7919f0346d32b1d02d587833d782c7c5
Ebeagu Samuel <samuel.ebeagu@toptal.com>
2023-10-06 11:10:31 +0100

    Add dependabot configuation (#104)

--- spec/lib/granite/action/performer_spec.rb
+++ spec/lib/granite/action/performer_spec.rb
@@ -1,6 +1,6 @@
 RSpec.describe Granite::Action::Performer do
-  let(:performer1) { instance_double(User, id: 5) }
-  let(:performer2) { instance_double(User, id: 10) }
+  let(:first_performer) { instance_double(User, id: 5) }
+  let(:second_performer) { instance_double(User, id: 10) }
 
   before do
     stub_class(:projector, Granite::Projector)
@@ -16,7 +16,7 @@ RSpec.describe Granite::Action::Performer do
   describe '#ctx' do
     specify { expect(Action.new.ctx).to be_nil }
     specify { expect(Action.with(performer: :value).new.ctx).to have_attributes(performer: :value) }
-    specify { expect(Action.as(performer1).new.ctx).to have_attributes(performer: performer1) }
+    specify { expect(Action.as(first_performer).new.ctx).to have_attributes(performer: first_performer) }
 
     specify 'proxy works for deeper initialization' do
       expect(Action.with(performer: :value).batch(2).map(&:ctx))
@@ -26,15 +26,15 @@ RSpec.describe Granite::Action::Performer do
 
   describe '#performer' do
     specify { expect(Action.new.performer).to be_nil }
-    specify { expect(Action.as(performer1).new.performer).to eq(performer1) }
+    specify { expect(Action.as(first_performer).new.performer).to eq(first_performer) }
 
     specify 'proxy works for deeper initialization' do
-      expect(Action.as(performer1).batch(2).map(&:performer)).to eq([performer1, performer1])
+      expect(Action.as(first_performer).batch(2).map(&:performer)).to eq([first_performer, first_performer])
     end
   end
 
   describe '#performer_id' do
     specify { expect(Action.new.performer_id).to be_nil }
-    specify { expect(Action.as(performer1).new.performer_id).to eq(performer1.id) }
+    specify { expect(Action.as(first_performer).new.performer_id).to eq(first_performer.id) }
   end
 end
